bc7a88038f9af7fcb6b49ed38ef2d6bd26501e9d
Gesley de Oliveira Rosa <geeh.oliveira@hotmail.com>
2024-02-29 22:20:08 -0300

    att

--- CalcularHorasTrabalhadas/src/HorasTrabalhadas/CalcularHorasTrabalhadas.java
+++ CalcularHorasTrabalhadas/src/HorasTrabalhadas/CalcularHorasTrabalhadas.java
@@ -1,10 +1,34 @@
 package HorasTrabalhadas;
 
 public class CalcularHorasTrabalhadas{
-    int horaTotal = 0;
+    double horaTotal = 0;
+    double horaExtra = 0;
+    double depoisDasDez = 0;
+    double aux = 0;
 
-    public int calculaHoras(int horaInicio, int diaInicio, int horaFim, int diaFim) {
+    public double calculaHoras(double horaInicio, double horaFim) {
         horaTotal = horaFim - horaInicio;
+        if(horaTotal > 6){
+            horaTotal += 1.5;
+        } if (horaFim > 18 && horaFim < 22){
+            horaExtra = horaFim - 18;
+            horaTotal = horaTotal - horaExtra;
+            horaTotal += horaExtra * 1.5;
+        } if (horaFim > 18 && horaFim > 22 && horaInicio < 18){
+            horaTotal = 18 - horaInicio;
+            depoisDasDez = (horaFim - 22) * 2;
+            horaTotal += depoisDasDez + 6;
+        } if (horaInicio < 8){
+            horaExtra = (8 - horaInicio) * 0.5;
+            horaTotal += horaExtra;  
+        } if (horaFim < 8 && horaFim > 6){
+            aux = 8 - horaFim;
+            horaTotal = horaTotal - aux;
+            horaExtra = aux * 2;
+            horaTotal = horaTotal + horaExtra;
+        } if (horaFim < 8 && horaInicio > 0){
+            horaTotal = horaTotal * 2;
+        }
         return horaTotal;
     }
     
--- CalcularHorasTrabalhadas/test/testeHorasTrabalhadas.java
+++ CalcularHorasTrabalhadas/test/testeHorasTrabalhadas.java
@@ -14,7 +14,14 @@ public class testeHorasTrabalhadas {
     @Test
     public void receberHorasTrabalhadasExibirCalculo(){
         c = new CalcularHorasTrabalhadas();
-        assertEquals(7, c.calculaHoras(1, 22, 8, 22));
+        /*1*/assertEquals(4.00, c.calculaHoras(8.00, 12.00), 0);
+        /*2*/assertEquals(5.00, c.calculaHoras(13.00, 18.00), 0);
+        /*3*/assertEquals(11.50, c.calculaHoras(8.00, 18.00), 0);
+        /*4*/assertEquals(5.50, c.calculaHoras(14.00, 19.00), 0);
+        /*5*/assertEquals(7.00, c.calculaHoras(6.00, 12.00), 0);
+        /*6*/assertEquals(9.50, c.calculaHoras(6.00, 13.00), 0);
+        /*7*/assertEquals(9.00, c.calculaHoras(17.00, 23.00), 0);
+
     }
     
 }
